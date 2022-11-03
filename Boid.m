99d3919a8fd54ff74561e02faf452b3f216e80bb
Justin Zhu <shuqinzhu37@gmail.com>
2022-11-03 16:50:54 -0700

    refine code

--- Boid.m
+++ Boid.m
@@ -6,30 +6,17 @@ classdef Boid < handle
         velocity = [0, 0, 0];                  % boid's velocity
         neighbors = [];                     % other boids that are close to the boid
         close_neighbors = [];               % other boids that are too close.
-        color_r = 0;                        %  boid's
-        color_g = 0;                        %  color
-        color_b = 0;                        %  values
         height = 0;                         % Height of the world.
         width = 0;                          % Width of the world.
         length = 0;                         % Length of the world.
         max_speed = 0;                      % Maximum speed of the boid.
-        check_range = 25;
-        rep_range = 5;
+        check_range = 25;                   % the distance that boid will check
+        rep_range = 5;                      % the distance that boid will repel one another
+        target = [0, 0, 0]                  % boid's target
+        priority = [1, 1, 1, 1, 1]          % Priority of rules 1 to 5
+        stepsPerSec = 25;                   % Define step length, number of steps per sec
     end
     methods
-        % Method that sets sets the height and width of the world for boid.
-        % Input: height of world, width of world
-        function obj = set_display(obj, length, width, height);
-            obj.height = height;
-            obj.width = width;
-            obj.length = length;
-        end
-        
-        % Method that sets the maximum speed of the boid.
-        % Input: maximum speed of boid
-        function obj = set_max_speed(obj, max_speed)
-            obj.max_speed = max_speed;
-        end
         
         % Method that updates the coordinate of the boid according to its
         % new velocity. The new velocity is adjusted by its previous
@@ -38,26 +25,115 @@ classdef Boid < handle
         % Input : Array of boids, Array of predators
         function obj = move(obj, boids)
             obj.findNeighbors(boids);
-            [v1x, v1y, v1z] = obj.avoid_edge();
-            [v2x, v2y, v2z] = obj.cohesion();
-            [v3x, v3y, v3z] = obj.separation();
-            [v4x, v4y, v4z] = obj.alignment();
+            v1 = obj.separation();
+            v2 = obj.alignment();
+            v3 = obj.cohesion();
+            v4 = obj.avoid_edge();
+%             v5 = obj.goTo_target();
+            v5 = [0, 0, 0];
+
             % New velocity is previous velocity plus change of velocity due
             % to the rules.
-            obj.velocity(1) = obj.velocity(1) + (v1x + v2x + v3x + v4x) / 2;
-            obj.velocity(2) = obj.velocity(2) + (v1y + v2y + v3y + v4y) / 2;
-            obj.velocity(3) = obj.velocity(3) + (v1z + v2z + v3z + v4z) / 2;
+            obj.velocity = obj.velocity + (obj.priority(1) * v1 + obj.priority(2) * v2 ...
+                + obj.priority(3) * v3 + obj.priority(4) * v4 + obj.priority(5) * v5) / obj.stepsPerSec;
 
             obj.limit_speed();
-            obj.coord(1) = obj.coord(1) + obj.velocity(1);
-            obj.coord(2) = obj.coord(2) + obj.velocity(2);
-            obj.coord(3) = obj.coord(3) + obj.velocity(3);
+            obj.coord = obj.coord + obj.velocity;
 
             % Reset the neighbors arrays for the next iteration.
             obj.neighbors = [];
             obj.close_neighbors = [];
         end
+
+        % Rule 1. Avoid collisions
+        % Method that calculates the velcotiy change due to
+        % separation factor. First, calculate the coordinate difference
+        % between the boid and its close neighbors, add them all, and
+        % average them. Them. change the sign of the vector so that it
+        % faces away from the other boids, and divide it by a coefficient.
+        function velocity = separation(obj)
+            goal_pos = [0, 0, 0];
+            if numel(obj.close_neighbors) == 0
+               velocity = [0, 0, 0];
+            else
+                for i = 1 : numel(obj.close_neighbors)
+                    neighbor = obj.close_neighbors(i);
+                    goal_pos = goal_pos - (neighbor.coord - obj.coord);
+                end
+                velocity = goal_pos / numel(obj.close_neighbors) / 2;
+            end
+        end
+         
+        % Rule 2. match speeds with neighbors and match heading
+        % Method that calculates the velocity change due to alignment
+        % factor. The method calculates the average velocity, divides it by
+        % a coefficient, and returns the result.
+        function velocity = alignment(obj)
+            if numel(obj.neighbors) == 0
+                velocity = [0, 0, 0];
+            else
+                avg_vector = [0, 0, 0];
+                for i = 1 : numel(obj.neighbors)
+                    neighbor = obj.neighbors(i);
+                    avg_vector = avg_vector + neighbor.velocity;
+                end
+                velocity = avg_vector / numel(obj.neighbors) / 4;
+            end
+        end
         
+        
+        % Rule 3. Move to the center of neighbors
+        % Method that calculates the velocity change due to cohesion
+        % factor. The method calculates the average position of its
+        % neighbors, and returns a vector that is from the boid's current
+        % position to the average position divided by a coefficient.
+        function velocity = cohesion(obj)
+            avg_position = [0, 0, 0];
+            % If there is no neighbor, then return [0, 0, 0].
+            if numel(obj.neighbors) == 0
+                velocity = [0, 0, 0];
+            else
+                for i = 1 : numel(obj.neighbors)
+                    avg_position = avg_position + obj.neighbors(i).coord;
+                end
+                avg_position = avg_position / numel(obj.neighbors);
+                velocity = (avg_position - obj.coord) / 20;
+            end
+        end
+       
+        % Rule 4. Method that makes the boid turn if it is close to the
+        % border. The method checks if the boid is approaching the edge,
+        % and if it is, then the method returns a vector which faces the
+        % opposite direction of the boid's direction.
+        function velocity = avoid_edge(obj)
+            velocity = [0, 0, 0];
+            border = [obj.length, obj.width, obj.height];
+            
+            for i = 1 : length(obj.coord)
+                % If the boid is reaching the upper border
+                if obj.coord(i) < border(i) / 10
+                    velocity(i) = 3;
+                    % If the boid is reaching the lower border
+                    % returned is [-1.5, 0, 0].
+                elseif obj.coord(i) > border(i) - (border(i) / 10)
+                    velocity(i) = -3;
+                end
+            end
+
+        end
+        
+        % Rule 5. Try to move to target
+        % Method that makes the boid head to its target while having rule
+        % 1,2 as higher priority
+        function velocity = goTo_target(obj)
+            velocity = [0, 0, 0];
+            distance = abs(norm(obj.coord - obj.target));
+            % Check if the target is in checking range
+            if distance <= obj.check_range
+                velocity = (obj.target - obj.coord) / 20;
+            end
+        end
+
         % method that finds other nearby Boids and remember the
         % information. The method calculates the distance between
         % itobj and all the other Boids, then store all Boids whose
@@ -84,118 +160,6 @@ classdef Boid < handle
             end
         end
         
-        % Rule 1. Method that calculates the velocity change due to cohesion
-        % factor. The method calculates the average position of its
-        % neighbors, and returns a vector that is from the boid's current
-        % position to the average position divided by a coefficient.
-        function [x, y, z] = cohesion(obj)
-            avg_position = [0, 0, 0];
-            % If there is no neighbor, then return [0, 0, 0].
-            if numel(obj.neighbors) == 0
-                x = 0;
-                y = 0;
-                z = 0;
-            else
-                for i = 1 : numel(obj.neighbors)
-                    avg_position = avg_position + obj.neighbors(i).coord;
-                end
-                avg_position = avg_position / numel(obj.neighbors);
-                x = (avg_position(1) - obj.coord(1)) / 20;
-                y = (avg_position(2) - obj.coord(2)) / 20;
-                z = (avg_position(3) - obj.coord(3)) / 20;
-            end
-        end
-        
-        % Rule 2. Method that calculates the velocity change due to alignment
-        % factor. The method calculates the average velocity, divides it by
-        % a coefficient, and returns the result.
-        function [x, y, z] = alignment(obj)
-            if numel(obj.neighbors) == 0
-                x = 0;
-                y = 0;
-                z = 0;
-            else
-                avg_vector = [0, 0, 0];
-                for i = 1 : numel(obj.neighbors)
-                    neighbor = obj.neighbors(i);
-                    avg_vector(1) = avg_vector(1) + neighbor.velocity(1);
-                    avg_vector(2) = avg_vector(2) + neighbor.velocity(2);
-                    avg_vector(3) = avg_vector(3) + neighbor.velocity(3);
-                end
-                x = avg_vector(1) / numel(obj.neighbors) / 4;
-                y = avg_vector(2) / numel(obj.neighbors) / 4;
-                z = avg_vector(3) / numel(obj.neighbors) / 4;
-            end
-        end
-        
-        % Rule 3. Method that calculates the velcotiy change due to
-        % separation factor. First, calculate the coordinate difference
-        % between the boid and its close neighbors, add them all, and
-        % average them. Them. change the sign of the vector so that it
-        % faces away from the other boids, and divide it by a coefficient.
-        function [x, y, z] = separation(obj)
-            goal_pos = [0, 0, 0];
-            if numel(obj.close_neighbors) == 0
-                x = 0;
-                y = 0;
-                z = 0;
-            else
-                for i = 1 : numel(obj.close_neighbors)
-                    neighbor = obj.close_neighbors(i);
-                    goal_pos(1) = goal_pos(1) -...
-                        (neighbor.coord(1) - obj.coord(1));
-                    goal_pos(2) = goal_pos(2) -...
-                        (neighbor.coord(2) - obj.coord(2));
-                    goal_pos(3) = goal_pos(3) -...
-                        (neighbor.coord(3) - obj.coord(3));
-                end
-                x = goal_pos(1) / numel(obj.close_neighbors) / 2;
-                y = goal_pos(2) / numel(obj.close_neighbors) / 2;
-                z = goal_pos(3) / numel(obj.close_neighbors) / 2;
-            end
-        end
-        
-        % Rule 4. Method that makes the boid turn if it is close to the
-        % border. The method checks if the boid is approaching the edge,
-        % and if it is, then the method returns a vector which faces the
-        % opposite direction of the boid's direction.
-        function [x, y, z] = avoid_edge(obj)
-            x = 0;
-            y = 0;
-            z = 0;
-            % If the boid is reaching the northern border, then the vector
-            % returned is [1.5, 0, 0].
-            if obj.coord(1) < obj.length / 10
-                x = 1.5;
-                % If the boid is reaching the southern border, then the vector
-                % returned is [-1.5, 0, 0].
-            elseif obj.coord(1) > obj.length - (obj.length / 10);
-                x = -1.5;
-            end
-            % If the boid is reaching the western border, then the vector
-            % returned is [0, 1.5, 0].
-            if obj.coord(2) < obj.width / 10
-                y = 1.5;
-                % If the boid is reaching the eastern border, then the vector
-                % returned is [0, -1.5, 0].
-            elseif obj.coord(2) > obj.width - (obj.width / 10);
-                y = -1.5;
-            end
-
-            % If the boid is reaching the western border, then the vector
-            % returned is [0, 0, 1.5].
-            if obj.coord(3) < obj.height / 10
-                z = 1.5;
-                % If the boid is reaching the eastern border, then the vector
-                % returned is [0, 0, -1.5].
-            elseif obj.coord(3) > obj.height - (obj.height / 10);
-                z = -1.5;
-            end
-
-        end
-        
-               
-        
         % Method that checks the speed of the boid, and reduces the speed
         % if it exceeds the limit speed.
         function obj = limit_speed(obj)
@@ -204,6 +168,27 @@ classdef Boid < handle
                 obj.velocity = obj.velocity * (obj.max_speed / curr_speed);
             end
         end
+        
+        % Method that sets sets the height and width of the world for boid.
+        % Input: height of world, width of world
+        function obj = set_display(obj, length, width, height);
+            obj.height = height;
+            obj.width = width;
+            obj.length = length;
+        end
+        
+        % Method that sets the maximum speed of the boid.
+        % Input: maximum speed of boid
+        function obj = set_max_speed(obj, max_speed)
+            obj.max_speed = max_speed;
+        end
+
+        % Method that sets the target to the boid.
+        % Input: target coordinate
+        function obj = set_target(obj, target)
+            obj.target = target;
+        end
+        
     end
 end
 
--- Boids.m
+++ Boids.m
@@ -1,12 +1,12 @@
 clear;
 close all;
 
-numBoids = 70;      % Number of boids to be simulated.
+numBoids = 90;      % Number of boids to be simulated.
 height = 100;       % Height of the world.
 width = 100;        % Width of the world.
 length = 100;        % length of the world.
 iteration = 1000;    % Number of simulation times.
-b_max_speed = 2;      % Maximum speed of boids.
+b_max_speed = 3;      % Maximum speed of boids.
 
 % Create an array of Boid objects.
 boids(numBoids) = Boid;
@@ -14,9 +14,6 @@ boids(numBoids) = Boid;
 
 % Initialize the boids with random color, coordinate, and velocity.
 for i = 1 : numBoids
-    boids(i).color_r = rand;
-    boids(i).color_g = rand;
-    boids(i).color_b = rand;
     boids(i).velocity = [rand * (b_max_speed * 2) - (b_max_speed / 2), rand * (b_max_speed * 2) - (b_max_speed / 2), rand * (b_max_speed * 2) - (b_max_speed / 2)];
     boids(i).coord = [(rand * length - 1) + 1, (rand * width - 1) + 1, (rand * height - 1) + 1];
     boids(i).set_display(length, width, height);
@@ -25,9 +22,9 @@ end
 
 
 h = plot3(0,0,0);
-xlim([0 100]);
-ylim([0 100]);
-zlim([0 100]);
+xlim([0, length]);
+ylim([0 width]);
+zlim([0 height]);
 
 
 arrows = [];
@@ -49,7 +46,7 @@ for t = 1 : iteration
     for i = 1 : numBoids
         arrow(arrows(i),'Start',waypointslastStep(i,:),'Stop',waypointsPerStep(i,:),'Length',3,'BaseAngle',20);
     end
-    pause(0.04);
+    pause(0.01);
 end
 
 
